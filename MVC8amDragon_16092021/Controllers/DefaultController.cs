8405683751bcb8e1e08cfee7010eece8061de64d
yawar2019 <yawarali17@gmail.com>
2021-09-27 14:51:34 +0530

    committed

--- MVC8amDragon_16092021/Controllers/DefaultController.cs
+++ MVC8amDragon_16092021/Controllers/DefaultController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC8amDragon_16092021.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,25 @@ namespace MVC8amDragon_16092021.Controllers
         // GET: Default
         public ActionResult Index()
         {
+            DbSuperstarEntities db = new Models.DbSuperstarEntities();
+            ViewBag.CountryName = new SelectList(db.Countries.ToList(), "CountryId", "CountryName");
+            return View(db.CustomControls.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Index(RegisterForm frm)
+        {
+            
+
             return View();
         }
+     
+        public ActionResult create()
+        {
+
+
+            return View();
+        }
+
     }
 }
